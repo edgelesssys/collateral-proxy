fe7d6904bae5cc587a060c6998792a76758059f9
Charlotte Hartmann Paludo <git@charlotteharludo.com>
2026-08-27 11:03:31 +0200

    nixpkgs: go 1.26.5 -> 1.26.6

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -6,10 +6,10 @@ final: prev:
 {
   go_1_26 = prev.go_1_26.overrideAttrs (
     finalAttrs: _prevAttrs: {
-      version = "1.26.5";
+      version = "1.26.6";
       src = final.fetchurl {
         url = "https://go.dev/dl/go${finalAttrs.version}.src.tar.gz";
-        hash = "sha256-SVvkvIcXasVnOS5bQRar2YRm0z17SdQedkzMaXay3EI=";
+        hash = "sha256-oHIcVMaIkBRI13rZs+x+p8R0cwdV/4kTgukuy5P/LLE=";
       };
     }
   );
